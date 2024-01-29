e258ef222704eb2d56298270991cf8b6d98e5425
José Antonio Cháirez Pantoja <jose.chairez.is@unipolidgo.edu.mx>
2024-01-29 14:08:53 -0600

    first commit

---
+++ screens/LandScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LandScreen() {
+  return (
+    <div>
+      <Text>
+        Ya me crecio la verga
+      </Text>
+    </div>
+  )
+}
+
+export default LandScreen
